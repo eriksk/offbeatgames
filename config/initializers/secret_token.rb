ef5b40b748c44a2142370515ded0249501a93f1f
Th3dz <erikskoglund88@gmail.com>
2012-02-09 21:37:07 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Offbeatgames::Application.config.secret_token = 'a28f1708076866843473f9432b43c6d7b7bc063e59bfa2828aadd30044aee6f35dc7d25b7b13fe58792cb7116e4a221d76b4ef6ec5378236755e35d9418ec1fd'
